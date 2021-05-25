69ca1fd573487684f03d07a791b0bbef06bd49ec
Tanya E <73319716+tkengelhart@users.noreply.github.com>
2021-05-25 11:41:57 -0500

    Create joins-solution.sql

---
+++ joins-solution.sql
@@ -0,0 +1,14 @@
+--Get all customers and their addresses.
+--Get all orders and their line items (orders, quantity and product).
+--Which warehouses have cheetos?
+--Which warehouses have diet pepsi?
+--Get the number of orders for each customer. NOTE: It is OK if those without orders are not included in results.
+--How many customers do we have?
+--How many products do we carry?
+--What is the total available on-hand quantity of diet pepsi?
+
+
+STRETCH GOALS
+--How much was the total cost for each order?
+--How much has each customer spent in total?
+--How much has each customer spent in total? Customers who have spent $0 should still show up in the table. It should say 0, not NULL (research coalesce).
