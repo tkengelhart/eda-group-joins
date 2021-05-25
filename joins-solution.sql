0a094c713c851710813dd917171e205926775afb
Tanya E <73319716+tkengelhart@users.noreply.github.com>
2021-05-25 14:01:42 -0500

    Update joins-solution.sql

--- joins-solution.sql
+++ joins-solution.sql
@@ -33,7 +33,7 @@ WHERE "products"."description" = 'cheetos';
 
 
 --Which warehouses have diet pepsi?
-NONE
+
 
 SELECT * FROM "products"
 JOIN
@@ -48,17 +48,39 @@ WHERE "products"."description" = 'diet pepsi';
 
 
 --Get the number of orders for each customer. NOTE: It is OK if those without orders are not included in results.
+SELECT count("first_name")
+FROM "customers"
+RIGHT JOIN
+"addresses"
+ON
+"customers"."id" = "addresses"."customer_id"
 
+JOIN
+"orders"
+ON
+"orders"."address_id" = "addresses"."id"
+
+GROUP BY "customers";
 
 
 
 --How many customers do we have?
 
+SELECT count(*) FROM "customers";
+
 
 --How many products do we carry?
-
+SELECT count(*) FROM "products";
 
 --What is the total available on-hand quantity of diet pepsi?
+this isn't right, will keep working on this one.
+
+SELECT count("warehouse_product"."on_hand") FROM "warehouse_product"
+ JOIN
+"products"
+ON
+"products"."id" = "warehouse_product"."product_id"
+WHERE "products"."description" = 'diet pepsi';
 
 
 STRETCH GOALS
